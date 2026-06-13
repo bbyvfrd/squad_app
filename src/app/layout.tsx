f1cc435fe452baaa9fe5857bc4909ebe23f4fd59
Farid Babayev <fbabaevv@gmail.com>
2026-06-13 15:11:35 +0400

    feat(design): next-themes provider on data-theme, light default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { karla, materialSymbols, montserrat } from "@/lib/fonts";
+import { Providers } from "@/components/providers";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,7 +17,9 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${montserrat.variable} ${karla.variable} ${materialSymbols.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
